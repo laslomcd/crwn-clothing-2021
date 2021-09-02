f6663d178ad72342fbc753d3ebc4eeb17710d6db
Ryan McDonagh <laslomcd@gmail.com>
2021-09-02 16:23:23 -0400

    added react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/Homepage';
 
+const HatPage = () => (
+  <div><h1>Hat Page</h1></div>
+);
+
 function App() {
   return(
-  <div>
-    <HomePage />
-  </div>
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatPage} />
+      </Switch>
+    </div>
   )
-}
+};
 
 export default App;
